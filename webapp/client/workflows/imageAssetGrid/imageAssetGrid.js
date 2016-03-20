545b5ed37962593b08fa08da5180a5ea7824d343
Abigail Watson <awatson1978@gmail.com>
2016-03-19 19:17:04 -0700

    desaturated images

--- webapp/client/workflows/imageAssetGrid/imageAssetGrid.js
+++ webapp/client/workflows/imageAssetGrid/imageAssetGrid.js
@@ -2,7 +2,7 @@ Template.imageAssetsGrid.helpers({
   image_assets: function(){
     return Meteor.users.find({
       'profile.name': { $regex: Session.get('imageNameFilter'), $options: 'i'}
-    });
+    },{limit: 30});
   },
   anatomy_image: function(){
     return this.profile.avatar;
